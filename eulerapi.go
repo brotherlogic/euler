705860fd06a0372918065d522566e3127d6b763f
Simon Tucker <brotherlogic@gmail.com>
2021-11-13 12:14:52 -0800

    Adds in stuffs

--- eulerapi.go
+++ eulerapi.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"golang.org/x/net/context"
 
@@ -10,5 +11,22 @@ import (
 
 //Solve a problem
 func (s *Server) Solve(ctx context.Context, req *pb.SolveRequest) (*pb.SolveResponse, error) {
+	st := time.Now()
+	resp := s.intSolve(req.GetProblemNumber())
+	if resp != nil {
+		resp.TimeTakenMillis = time.Since(st).Milliseconds()
+	}
+
 	return nil, fmt.Errorf("Cannot solve that problem")
+}
+
+func (s *Server) intSolve(ctx context.Context, number int32) *pb.SolveResponse {
+	switch number {
+	case 1:
+		return &pb.SolveResponse{
+			Numerator: int64(s.solve1(ctx, 1000)),
+		}
+	}
+
+	return nil
 }
